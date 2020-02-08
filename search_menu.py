ff1f117f862edd998edd9538b4f2a165a9b89894
Vijay Pappu <vpappu@meetup.com>
2020-02-08 14:19:03 -0500

    added comments 3

--- search_menu.py
+++ search_menu.py
@@ -6,25 +6,34 @@ from whoosh.query import Term
 
 
 class SearchZendesk:
+    """
+    Class to search users, tickets and organizations
+    """
     def __init__(self):
+        """
+        Constructor for SearchZendesk class
+        """
         self.main_choices = {
             1: "Select 1 to search Zendesk",
             2: "Select 2 to view a list of searchable fields",
             3: "Exit"
-        }
+        }  # main menu choices
 
         self.sub_choices = {
             1: "Select 1 to search users",
             2: "Select 2 to search tickets",
             3: "Select 3 to search organizations",
             4: "Select 4 to go back to main menu"
-        }
+        }  # search menu choices
 
-        self.user = User()
-        self.ticket = Ticket()
-        self.organization = Organization()
+        self.user = User()  # instantiates user class
+        self.ticket = Ticket()  # instantiates ticket class
+        self.organization = Organization()  # instantiates organization class
 
     def print_main_menu(self):
+        """
+        prints main menu
+        """
         print(2 * "*", f"{Colors.BOLD}Select options: {Colors.ENDC}", 2 * "*", "\n")
         for (k, v) in self.main_choices.items():
             print(str(k) + ". " + v)
@@ -32,6 +41,9 @@ class SearchZendesk:
         print(66 * "-")
 
     def print_sub_menu(self):
+        """
+        prints search menu
+        """
         print("\n")
         print(2 * "*", f"{Colors.BOLD}Select from the following menu: {Colors.ENDC}", 2 * "*", "\n")
         for (k, v) in self.sub_choices.items():
@@ -41,11 +53,19 @@ class SearchZendesk:
 
     @staticmethod
     def print_results(results):
+        """
+        prints search results
+        :param results: list of search results
+        """
         for res in results:
             print("\n".join([(key + (30 - len(key)) * " " + str(value)) for (key, value) in res.items()]))
             print(80 * "-")
 
     def get_menu_response(self):
+        """
+        main method to input search queries and retrieve results
+        :return: search results
+        """
         print("\n")
         print(23 * "*", f"{Colors.BOLD}{Colors.BLUE}Welcome to Zendesk Search{Colors.ENDC}", 23 * "*")
         print("\n")
@@ -53,39 +73,39 @@ class SearchZendesk:
         main_loop = True
         sub_loop = True
 
-        while main_loop:
-            self.print_main_menu()
+        while main_loop:  # outer loop to receive main menu options
+            self.print_main_menu()  # print main menu
             try:
                 choice = int(input("Enter your choice [" + str(min(self.main_choices.keys())) + "-" + str(max(self.main_choices.keys())) + "]: "))
                 if choice == 1:
                     while sub_loop:
-                        self.print_sub_menu()
+                        self.print_sub_menu()  # print sub menu
                         try:
                             sub_choice = int(input("Enter your choice [" + str(min(self.sub_choices.keys())) + "-" + str(max(self.sub_choices.keys())) + "]: "))
-                            if sub_choice == 1:
+                            if sub_choice == 1:  # search users
                                 query = self.get_query(self.user.fields)
                                 results = self.search_users(query)
                                 self.print_results(results)
-                            elif sub_choice == 2:
+                            elif sub_choice == 2:  # search tickets
                                 query = self.get_query(self.ticket.fields)
                                 results = self.search_tickets(query)
                                 self.print_results(results)
-                            elif sub_choice == 3:
+                            elif sub_choice == 3:  # search organization
                                 query = self.get_query(self.organization.fields)
                                 results = self.search_organizations(query)
                                 self.print_results(results)
-                            elif sub_choice == 4:
+                            elif sub_choice == 4:  # terminate sub loop
                                 sub_loop = False
                             else:
                                 input("Wrong option selection. Enter any key to try again..")
                         except ValueError:
                             input("Wrong option selection. Enter any key to try again..")
                 elif choice == 2:
-                    self.user.print_fields()
-                    self.ticket.print_fields()
-                    self.organization.print_fields()
+                    self.user.print_fields()  # prints user fields
+                    self.ticket.print_fields()  # prints ticket fields
+                    self.organization.print_fields()  # prints organization fields
                 elif choice == 3:
-                    main_loop = False
+                    main_loop = False  # terminate main loop
                 else:
                     input("Wrong option selection. Enter any key to try again..")
                 print("\n")
@@ -93,16 +113,36 @@ class SearchZendesk:
                 input("Wrong option selection. Enter any key to try again..")
 
     def search_users(self, query):
+        """
+        searches users for the given query
+        :param query: Term query
+        :return: search results
+        """
         return self.user.search(query)
 
     def search_tickets(self, query):
+        """
+        searches tickets for the given query
+        :param query: Term query
+        :return: search results
+        """
         return self.ticket.search(query)
 
     def search_organizations(self, query):
+        """
+        searches organizations for the given query
+        :param query: Term query
+        :return: search results
+        """
         return self.organization.search(query)
 
     @staticmethod
     def get_query(fields):
+        """
+        Returns a term query from user input
+        :param fields: list of searchable fields
+        :return: Term query
+        """
         field = input("Enter search term:")
         while field not in fields:
             print("Search term not found. Choose a search term from the following fields:\n" + "\n".join(fields) + "\n")
@@ -112,6 +152,5 @@ class SearchZendesk:
 
 
 if __name__ == "__main__":
-
     SearchZendesk().get_menu_response()
 
